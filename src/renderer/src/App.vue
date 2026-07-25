31118689645b6bae4de3746714ee8ecd2b6b6e5d
Liang <liang@in-x.cc>
2026-07-25 09:12:25 +0800

    chore: 初始化 Electron + Vue 项目

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+</script>
+
+<template>
+  <button @click="ipcHandle">Send IPC</button>
+</template>
